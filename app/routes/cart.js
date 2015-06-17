cfef48ba0fe3fb0a2c71172cea0cb526040d07a8
ben Cornelis & Peter <student@epicodus.com>
2015-06-17 16:56:47 -0700

    use cart without outlet

--- app/routes/cart.js
+++ app/routes/cart.js
@@ -1,9 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  renderTemplate: function() {
-    this.render({ outlet: "cart"});
-  },
   model: function(params) {
     return this.store.find('cart', params.cart_id);
   }
